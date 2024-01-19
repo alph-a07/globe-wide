9eae092ddc87351f0875f34d8a8d2d1f5d6ba569
Jeel <itsjeel01@gmail.com>
2024-01-19 15:52:13 +0530

    Handled DELETE tour

--- app.js
+++ app.js
@@ -70,6 +70,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (+req.params.id > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID',
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);

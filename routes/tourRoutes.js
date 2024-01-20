fa0c5928579cd7ba43bf6cd50fcb8e7dd31e1c87
Jeel <itsjeel01@gmail.com>
2024-01-20 15:32:59 +0530

    Implemented new tour validation middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(tourController.checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;

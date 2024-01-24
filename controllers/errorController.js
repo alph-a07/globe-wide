22dcf9df011df1539555132396e2446dfcca8856
Jeel <itsjeel01@gmail.com>
2024-01-24 15:21:14 +0530

    Abstracted production errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,36 @@
+const sendErrorDev = (err, res) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        error: error,
+        message: error.message,
+        stack: error.stack,
+    });
+};
+
+const sendErrorProd = (err, res) => {
+    // Operational errors - Inform client
+    if (err.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+        });
+    }
+    // Unknown errors - Abstract from client
+    else {
+        console.log('ERROR 💥', err); // For developers
+
+        // Client abstraction
+        res.status(500).json({
+            status: 'error',
+            message: 'Unexpected error occured!',
+        });
+    }
+};
+
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
 
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message,
-    });
+    if (process.env.NODE_ENV === 'development') sendErrorDev(req, res);
+    else if (process.env.NODE_ENV === 'development') sendErrorProd(req, res);
 };

2997d0c531cea9280b0d195eabd08bb1afde9fd5
Jeel <itsjeel01@gmail.com>
2024-01-24 15:57:09 +0530

    Updated production errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,18 +1,33 @@
+const AppError = require('../utils/appError');
+
+const handleCastError = err => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+
+    return new AppError(message, 400);
+};
+
+const handleDuplicateEntry = err => {
+    const value = err.message.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+    const message = `Field already exists: ${value}`;
+
+    return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
-    res.status(error.statusCode).json({
-        status: error.status,
-        error: error,
-        message: error.message,
-        stack: error.stack,
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
     });
 };
 
 const sendErrorProd = (err, res) => {
     // Operational errors - Inform client
     if (err.isOperational) {
-        res.status(error.statusCode).json({
-            status: error.status,
-            message: error.message,
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
         });
     }
     // Unknown errors - Abstract from client
@@ -27,10 +42,17 @@ const sendErrorProd = (err, res) => {
     }
 };
 
-module.exports = (error, req, res, next) => {
-    error.statusCode = error.statusCode || 500;
-    error.status = error.status || 'error';
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'err';
 
-    if (process.env.NODE_ENV === 'development') sendErrorDev(req, res);
-    else if (process.env.NODE_ENV === 'development') sendErrorProd(req, res);
+    if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+    else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+
+        if (err.name === 'CastError') error = handleCastError(err);
+        if (err.code === 11000) error = handleDuplicateEntry(err);
+
+        sendErrorProd(error, res);
+    }
 };

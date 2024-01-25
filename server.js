1a7ab486642d336b23a4ece2fd5a60641ba71635
Jeel <itsjeel01@gmail.com>
2024-01-25 14:12:46 +0530

    Added safety nets

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+// Safety net - handles all unhandled synchronous exceptions
+process.on('uncaughtException', err => {
+    console.log('🚫 UNHANDLED EXCEPTION - Shutting down...');
+    console.log(err.name, ':', err.message);
+
+    process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
@@ -10,6 +18,19 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 mongoose.connect(DB).then(_ => console.log('Connected to DB!'));
 
 const port = process.env.port || 8080;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
+// Safety net - handles all unhandled promises
+process.on('unhandledRejection', err => {
+    console.log('🚫 UNHANDLED REJECTION - Shutting down...');
+    console.log(err.name, err.message);
+
+    // Close the app after server is closed!
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
+console.log(x);

1c0a9316f56583ba76da354507d3bfa046579139
Jeel <itsjeel01@gmail.com>
2024-01-25 14:39:08 +0530

    Implemented User model

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const { signup } = require('../controllers/authController');
+
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } = require('./../controllers/userController');
+
+router.post('/signup', signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);

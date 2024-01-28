fdf960633352ab952c56fb362e1cd726f71d40bb
Jeel <itsjeel01@gmail.com>
2024-01-28 13:34:01 +0530

    Implemented rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
@@ -9,8 +10,17 @@ const globalErrorController = require('./controllers/errorController');
 const app = express();
 
 app.use(express.json());
+
 // Use logger only in development environment
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// API Rate limiting middleware
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // MOUNT ROUTERS
 app.use('/api/v1/tours', tourRouter);

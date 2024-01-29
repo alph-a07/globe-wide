ce97d6086f7021158ac9db6dc1f66bcf01df0c53
Jeel <itsjeel01@gmail.com>
2024-01-29 19:05:24 +0530

    Populated tours and users while fetching reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,20 @@ const reviewSchema = mongoose.Schema(
     },
 );
 
+reviewSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'tour',
+        select: 'name',
+    });
+
+    this.populate({
+        path: 'user',
+        select: 'name photo',
+    });
+
+    next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
